d451499a215cea9d4775a41e5c6332cfa29605b4
Manuel Martinez <lucentum@gmail.com>
2017-04-28 12:22:03 +0200

    for more fun swing poking with concurrent SwingApp actor

---
+++ src/test/java/com/ittera/cometa/SwingAppConcurrentActor.java
@@ -0,0 +1,112 @@
+package com.ittera.cometa;
+
+import com.ittera.cometa.concentrator.DualPeer;
+import com.ittera.cometa.concentrator.messages.DataMessageBuilder;
+import com.ittera.cometa.concentrator.messages.protobuf.ProtobufDataMessageBuilder;
+import com.ittera.cometa.concentrator.messages.protobuf.data.Primitives;
+import com.ittera.cometa.concentrator.messages.protobuf.data.Wrappers.DataMessage;
+import com.ittera.cometa.concentrator.messages.protobuf.data.Wrappers.Type;
+
+import java.io.IOException;
+
+public class SwingAppConcurrentActor {
+
+    protected static DataMessageBuilder dataMessageBuilder = new ProtobufDataMessageBuilder(null);
+
+    protected static final String swingAppClassName = "com.ittera.cometa.apps.SwingApp";
+
+    private static class SwingActor implements Runnable {
+        DualPeer dualPeer;
+        final String jframeRef;
+
+        SwingActor(String jframeRef) {
+            this.jframeRef = jframeRef;
+            try {
+                this.dualPeer = new DualPeer("/tests.properties");
+            } catch (IOException ioe) {
+                System.err.println("error starting dual peer");
+                ioe.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 15; i++) {
+                sleep(300);
+
+                setFrameVisible(i % 2 == 0);
+            }
+            // finalize
+            dualPeer.close();
+        }
+
+        private void setFrameVisible(boolean visible) {
+            String methodName = "setVisible";
+            Object[] parameters = new Object[]{false};
+            String[] parameterTypesNamesArray = new String[]{"boolean"};
+            DataMessage requestMsg, replyMsg;
+            String fieldClassName = "javax.swing.JFrame";
+
+            parameters = new Object[]{visible};
+            requestMsg = dataMessageBuilder.buildInstanceMethod(dualPeer.getPeerUuid(), fieldClassName,
+                    methodName, jframeRef, parameterTypesNamesArray, parameters, new String[parameters.length]);
+            replyMsg = dualPeer.sendAndReceive(requestMsg);
+
+        }
+
+        protected static void sleep(long millisecs) {
+            try {
+                Thread.sleep(millisecs);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        final DualPeer dualPeer = new DualPeer("/tests.properties");
+        String methodName;
+
+        methodName = "main";
+        Class[] parameterTypes = new Class[]{String[].class};
+        String[] parameterTypesNamesArray = new String[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++) {
+            parameterTypesNamesArray[i] = parameterTypes[i].getName();
+        }
+        Object[] parameters = new Object[]{new String[]{}};
+
+
+        final DataMessage mainRequest = dataMessageBuilder.buildClassMethod(dualPeer.getPeerUuid(),
+                swingAppClassName, methodName, parameterTypesNamesArray, parameters, new String[parameterTypes.length]);
+
+        // start the swingapp by calling main in background
+        Thread asyncSend = new Thread() {
+            @Override
+            public void run() {
+                dualPeer.sendToLogAndForget(mainRequest);
+            }
+        };
+        asyncSend.start();
+
+        // wait for put of JFrame field;
+        String fieldName = "frame";
+        dualPeer.waitFor(Type.PUT_STATIC_DONE, fieldName);
+
+        // now get the jframe
+        DataMessage requestMsg = dataMessageBuilder.buildGetStatic(dualPeer.getPeerUuid(), swingAppClassName, fieldName);
+        DataMessage replyMsg = dualPeer.sendAndReceive(requestMsg);
+        Primitives.Object myFrame = replyMsg.getReturnValue().getObject();
+
+        // start some actors and pass them the frame to play with
+        final Thread[] actors = new Thread[5];
+        for (int i = 0; i < actors.length; i++) {
+            actors[i] = new Thread(new SwingActor(myFrame.getRef()));
+            // introduce some delay and start
+            Thread.sleep(150);
+            actors[i].start();
+        }
+
+        dualPeer.close();
+    }
+}
