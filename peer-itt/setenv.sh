e95fad7ca164816ac9b84d36344bcecd9eff48b4
Manuel Martinez <lucentum@gmail.com>
2019-07-04 20:32:00 +0200

    libraries upgraded to latest version; #151

--- peer-itt/setenv.sh
+++ peer-itt/setenv.sh
@@ -2,8 +2,8 @@ WORKING_DIR=`pwd`
 
 MVN_REPO=~/.m2/repository
 
-COMMONS_PATH=$MVN_REPO/org/apache/commons/commons-lang3/3.4/commons-lang3-3.4.jar
-ASPECTJ_PATH=$MVN_REPO/org/aspectj/aspectjrt/1.8.9/aspectjrt-1.8.9.jar
+COMMONS_PATH=$MVN_REPO/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar
+ASPECTJ_PATH=$MVN_REPO/org/aspectj/aspectjrt/1.9.4/aspectjrt-1.9.4.jar
 ASPECTJ_TOOLS=/usr/local/lib/aspectj/lib/aspectjtools.jar
 CORE_CLASSES=$MVN_REPO/com/ittera/cometa/peer-core/1.0-SNAPSHOT/peer-core-1.0-SNAPSHOT.jar
 COMMON_CLASSES=$MVN_REPO/com/ittera/cometa/peer-common/1.0-SNAPSHOT/peer-common-1.0-SNAPSHOT.jar
