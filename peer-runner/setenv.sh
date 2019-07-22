28530a2406813ed08a25d626e1a85dc921341477
Manuel Martinez <lucentum@gmail.com>
2019-07-22 21:11:54 +0200

    merged setenv files into unique at root folder

--- peer-runner/setenv.sh
+++
@@ -1,7 +0,0 @@
-
-WORKING_DIR=`pwd`
-
-# Add bin folder to path
-export PATH=$WORKING_DIR/bin:$JAVA_HOME/bin:$PATH
-
-export ZOOKEEPER_URL=localhost:2181
--- setenv.sh
+++ setenv.sh
@@ -1,13 +1,16 @@
 
 WORKING_DIR=`pwd`
 
+PEER_CORE_HOME=$WORKING_DIR/peer-core
+PEER_RUNNER_HOME=$WORKING_DIR/peer-runner
+
 # Paths needed by some tools in bin/
 export JLINE_HOME='/usr/share/java'
-export SJK_PATH=$WORKING_DIR/tools/lib/sjk-plus-0.5.1.jar
 export KAFKA_HOME='/usr/local/lib/kafka'
+export SJK_PATH=$PEER_CORE_HOME/tools/lib/sjk-plus-0.5.1.jar
 
 # Add bin folders to path
-export PATH=$WORKING_DIR/bin:$JAVA_HOME/bin:$PATH
+export PATH=$PEER_CORE_HOME/bin:$PEER_RUNNER_HOME/bin:$JAVA_HOME/bin:$PATH
 
 # Some usefull aliases to work with peer (in and outside containers)
 alias k='pkill -9 -f Concentrator'
@@ -15,5 +18,5 @@ alias kd='docker kill peer'
 alias dp='docker run --network peers --publish 5671:5671 --rm --name peer peer'
 alias da='docker exec -ti $(docker ps -f name=^/peer$ -q) /bin/sh'
 
-# For convenience; at least needed for running integration tests (peer-itt)
+# ENV variable needed by peer, runner and integration tests (peer-itt)
 export ZOOKEEPER_URL=localhost:2181
