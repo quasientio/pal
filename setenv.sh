a5cc8f28e5c9d9d429a0debdc92db6c209ff9ed2
Manuel Martinez <lucentum@gmail.com>
2019-07-22 21:39:09 +0200

    fixed typo in comment

--- setenv.sh
+++ setenv.sh
@@ -12,7 +12,7 @@ export SJK_PATH=$PEER_CORE_HOME/tools/lib/sjk-plus-0.5.1.jar
 # Add bin folders to path
 export PATH=$PEER_CORE_HOME/bin:$PEER_RUNNER_HOME/bin:$JAVA_HOME/bin:$PATH
 
-# Some usefull aliases to work with peer (in and outside containers)
+# Some useful aliases to work with peer (in and outside containers)
 alias k='pkill -9 -f Concentrator'
 alias kd='docker kill peer'
 alias dp='docker run --network peers --publish 5671:5671 --rm --name peer peer'
