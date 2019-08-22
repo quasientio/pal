c7803d93ee63427de85a214ea488b042b309a268
Manuel Martinez <lucentum@gmail.com>
2019-08-22 18:40:50 +0200

    zeromq POC unit tests

--- peer-core/src/test/java/com/ittera/cometa/concentrator/RouterDealerDispatchPOCTest.java
+++ peer-core/src/test/java/com/ittera/cometa/concentrator/RouterDealerDispatchPOCTest.java
@@ -18,7 +18,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
-public class RouterDealerDispatchTest {
+public class RouterDealerDispatchPOCTest {
 
 	/*
 	 a class for Workers (which REPly to Dealer)
