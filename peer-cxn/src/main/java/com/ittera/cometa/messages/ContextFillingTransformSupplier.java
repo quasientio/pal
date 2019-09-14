7ddf1db9e0792a946b3dca999c5a7603ff81810e
Manuel Martinez <lucentum@gmail.com>
2019-09-14 21:55:50 +0200

    stream-processing classes originally in tools project

---
+++ peer-cxn/src/main/java/com/ittera/cometa/messages/ContextFillingTransformSupplier.java
@@ -0,0 +1,34 @@
+package com.ittera.cometa.messages;
+
+import com.ittera.cometa.messages.protobuf.data.Wrappers.ExecMessage;
+
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Transformer;
+import org.apache.kafka.streams.processor.ProcessorContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContextFillingTransformSupplier implements Transformer<String, ExecMessage, KeyValue<String, Map>> {
+	private ProcessorContext processorContext;
+
+	@Override
+	public void init(ProcessorContext processorContext) {
+		this.processorContext = processorContext;
+	}
+
+	@Override
+	public KeyValue<String, Map> transform(String key, ExecMessage execMessage) {
+		Map<String, Object> map = new HashMap<>();
+		MessageContext messageContext = new MessageContext(processorContext.offset(), processorContext.partition(),
+			processorContext.timestamp(), processorContext.topic(), processorContext.headers());
+		map.put("message", execMessage);
+		map.put("context", messageContext);
+		return new KeyValue<>(key, map);
+	}
+
+	@Override
+	public void close() {
+
+	}
+}
