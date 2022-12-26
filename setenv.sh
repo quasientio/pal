af1e8ff4eb0697ca8a8cd49b68696b77bd324c23
Manuel Martinez <lucentum@gmail.com>
2022-12-26 20:27:47 +0100

    remove export of PATH and PAL_HOME from setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-WORKING_DIR=`pwd`
-
-#############
-# PATH exports
-#
-export PAL_HOME=$WORKING_DIR
-
 # Paths needed by some tools in bin/
 export JLINE_HOME='/usr/share/java'
 export KAFKA_HOME='/usr/local/lib/kafka'
 export SJK_PATH=$PAL_HOME/peer/tools/lib/sjk-plus-0.5.1.jar
-
-# Add bin folders to path
-export PATH=bin:$PAL_HOME/peer/bin:$JAVA_HOME/bin:$PATH
