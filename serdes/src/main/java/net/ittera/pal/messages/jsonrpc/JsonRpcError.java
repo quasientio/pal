6afd3a0a83a1056c81e90b0d6cf29d483dcaeae3
Manuel Martinez <lucentum@gmail.com>
2024-04-20 16:15:47 +0200

    Move JsonRpcError under `messages` package

--- serdes/src/main/java/net/ittera/pal/messages/jsonrpc/JsonRpcError.java
+++ serdes/src/main/java/net/ittera/pal/messages/jsonrpc/JsonRpcError.java
@@ -1,15 +1,16 @@
-package net.ittera.pal.serdes.jsonrpc;
+package net.ittera.pal.messages.jsonrpc;
 
 public class JsonRpcError {
 
-  private int
-      code; // A Number that indicates the error type that occurred. This MUST be an integer.
-  private String
-      message; // A string providing a short description of the error. The message SHOULD be limited
-  // to a concise single sentence.
+  // A Number that indicates the error type that occurred. This MUST be an integer.
+  private final int code;
 
-  private Object
-      data; // A Primitive or Structured value that contains additional information about the error.
+  // A string providing a short description of the error. The message SHOULD be limited to a concise
+  // single sentence.
+  private final String message;
+
+  // A Primitive or Structured value that contains additional information about the error.
+  private final Object data;
 
   public JsonRpcError(int code, String message, Object data) {
     this.code = code;
