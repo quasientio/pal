bdc5debad1a609d758c9f5739e34b6d4f07f8d77
Manuel Martinez <lucentum@gmail.com>
2024-04-27 12:44:26 +0200

    Remove unused class

--- serdes/src/test/java/net/ittera/pal/messages/jsonrpc/ClassForTestingBeanutils.java
+++
@@ -1,7 +0,0 @@
-package net.ittera.pal.messages.jsonrpc;
-
-public class ClassForTestingBeanutils {
-  public void methodWithBoolParameter(boolean value) {
-    System.out.println("methodWithBoolParameter called with value: " + value);
-  }
-}
