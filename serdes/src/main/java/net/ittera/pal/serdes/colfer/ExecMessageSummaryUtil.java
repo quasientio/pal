11dc0dea0bc3e8236d0ce1e0a9b6646fc1b6a5e8
Manuel Martinez <lucentum@gmail.com>
2024-12-25 15:47:24 +0100

    Trim and escape EOLs in exec msg summary

--- serdes/src/main/java/net/ittera/pal/serdes/colfer/ExecMessageSummaryUtil.java
+++ serdes/src/main/java/net/ittera/pal/serdes/colfer/ExecMessageSummaryUtil.java
@@ -11,6 +11,8 @@ import net.ittera.pal.serdes.RpcMessageSummaryUtil;
 
 public class ExecMessageSummaryUtil extends RpcMessageSummaryUtil {
 
+  private static final short SUMMARY_MAX_LENGTH = 100;
+
   private static String getObjRepr(Obj obj, String objectRef) {
     return getObjRepr(
         obj == null ? null : obj.isNull,
@@ -29,66 +31,98 @@ public class ExecMessageSummaryUtil extends RpcMessageSummaryUtil {
 
   public static String getOneLinerSummary(ExecMessage msg) {
     final MessageType execMessageType = getMessageTypeOf(msg);
+    final String summary;
     switch (execMessageType) {
       case EXEC_CONSTRUCTOR:
-        return "new " + classname(msg);
+        summary = "new " + classname(msg);
+        break;
       case EXEC_INSTANCE_METHOD:
-        return String.format(
-            "call %s.%s@%s",
-            classname(msg), msg.instanceMethodCall.name, msg.instanceMethodCall.objectRef);
+        summary =
+            String.format(
+                "call %s.%s@%s",
+                classname(msg), msg.instanceMethodCall.name, msg.instanceMethodCall.objectRef);
+        break;
       case EXEC_CLASS_METHOD:
-        return String.format("call %s.%s", classname(msg), msg.classMethodCall.name);
+        summary = String.format("call %s.%s", classname(msg), msg.classMethodCall.name);
+        break;
       case EXEC_GET_STATIC:
-        return String.format("get %s.%s", classname(msg), msg.staticFieldGet.field.name);
+        summary = String.format("get %s.%s", classname(msg), msg.staticFieldGet.field.name);
+        break;
       case EXEC_GET_FIELD:
-        return String.format(
-            "get %s.%s@%s",
-            classname(msg), msg.instanceFieldGet.field.name, msg.instanceFieldGet.objectRef);
+        summary =
+            String.format(
+                "get %s.%s@%s",
+                classname(msg), msg.instanceFieldGet.field.name, msg.instanceFieldGet.objectRef);
+        break;
       case EXEC_PUT_STATIC:
-        return String.format(
-            "put %s.%s ⇦ %s",
-            classname(msg),
-            msg.staticFieldPut.field.name,
-            getObjRepr(msg.staticFieldPut.valueObject, msg.staticFieldPut.valueObjectRef));
+        summary =
+            String.format(
+                "put %s.%s ⇦ %s",
+                classname(msg),
+                msg.staticFieldPut.field.name,
+                getObjRepr(msg.staticFieldPut.valueObject, msg.staticFieldPut.valueObjectRef));
+        break;
       case EXEC_PUT_FIELD:
-        return String.format(
-            "put %s.%s@%s ⇦ %s",
-            classname(msg),
-            msg.instanceFieldPut.field.name,
-            msg.instanceFieldPut.objectRef,
-            getObjRepr(msg.instanceFieldPut.valueObject, msg.instanceFieldPut.valueObjectRef));
+        summary =
+            String.format(
+                "put %s.%s@%s ⇦ %s",
+                classname(msg),
+                msg.instanceFieldPut.field.name,
+                msg.instanceFieldPut.objectRef,
+                getObjRepr(msg.instanceFieldPut.valueObject, msg.instanceFieldPut.valueObjectRef));
+        break;
       case EXEC_PUT_STATIC_DONE:
-        return String.format("put_done %s.%s", classname(msg), msg.staticFieldPutDone.field.name);
+        summary =
+            String.format("put_done %s.%s", classname(msg), msg.staticFieldPutDone.field.name);
+        break;
       case EXEC_PUT_FIELD_DONE:
-        return String.format("put_done %s.%s", classname(msg), msg.instanceFieldPutDone.field.name);
+        summary =
+            String.format("put_done %s.%s", classname(msg), msg.instanceFieldPutDone.field.name);
+        break;
       case EXEC_THROWABLE:
-        return String.format(
-            "throw %s: \"%s\"", classname(msg), msg.raisedThrowable.throwable.message);
+        summary =
+            String.format(
+                "throw %s: \"%s\"", classname(msg), msg.raisedThrowable.throwable.message);
+        break;
       case EXEC_RETURN_VALUE:
         if (msg.returnValue.isVoid) {
-          return "return void";
+          summary = "return void";
         } else {
           if (msg.getReturnValue().getFrom().getConstructor() != null) {
-            return String.format(
-                "return new %s%s", classname(msg), getObjRepr(msg.returnValue.object));
+            summary =
+                String.format(
+                    "return new %s%s", classname(msg), getObjRepr(msg.returnValue.object));
           } else if (msg.getReturnValue().getFrom().getField() != null) {
             Field field = msg.getReturnValue().getFrom().getField();
             String fieldName = null;
             if (field != null && field.name != null && !field.name.isEmpty()) {
               fieldName = "(" + msg.getReturnValue().getFrom().getField().name + ")";
             }
-            return String.format(
-                    "return %s%s %s",
-                    classname(msg),
-                    getObjRepr(msg.returnValue.object),
-                    fieldName != null ? fieldName : "")
-                .trim();
+            summary =
+                String.format(
+                        "return %s%s %s",
+                        classname(msg),
+                        getObjRepr(msg.returnValue.object),
+                        fieldName != null ? fieldName : "")
+                    .trim();
           } else {
-            return String.format("return %s%s", classname(msg), getObjRepr(msg.returnValue.object));
+            summary =
+                String.format("return %s%s", classname(msg), getObjRepr(msg.returnValue.object));
           }
         }
+        break;
       default:
-        return "UNKNOWN MESSAGE TYPE";
+        summary = "UNKNOWN MESSAGE TYPE";
     }
+
+    // escape EOL chars
+    String eolEscaped = summary.replaceAll("\\r?\\n", "\\\\n");
+
+    // return, trimming to max length
+    return trimToMaxLength(eolEscaped);
+  }
+
+  private static String trimToMaxLength(String str) {
+    return str.length() > SUMMARY_MAX_LENGTH ? str.substring(0, SUMMARY_MAX_LENGTH) : str;
   }
 }
