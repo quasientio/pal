298f88342e8b26b41723fec965292ed5bf6d2fbe
Manuel Martinez <lucentum@gmail.com>
2025-06-08 15:18:29 +0200

    Fix port issues - advertised listener - and verify required ports when starting kafka on docker

--- infra/bin/start_kafka_docker.sh
+++ infra/bin/start_kafka_docker.sh
@@ -11,6 +11,11 @@ check_var "PAL_AUTOMATE"
 # Load env variables
 source "$PAL_AUTOMATE/docker/.env"
 
+# Check all required variables are defined
+check_var "KAFKA_PORT"
+check_var "KAFKA_HOST_PORT"
+check_var "KAFKA_CONTROLLER_PORT"
+
 # Check if a container named "kafka" is running
 if [ "$(docker ps -q -f name=kafka)" ]; then
   log "kafka container already running"
@@ -25,12 +30,26 @@ fi
 createPalNetwork
 
 # Initialize Docker run options
-DOCKER_RUN_OPTS=""
+DOCKER_RUN_OPTS="\
+  -e KAFKA_PROCESS_ROLES=broker,controller \
+  -e KAFKA_NODE_ID=1 \
+  -e KAFKA_CONTROLLER_QUORUM_VOTERS=1@kafka:${KAFKA_CONTROLLER_PORT} \
+  -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:${KAFKA_PORT},PLAINTEXT_HOST://0.0.0.0:${KAFKA_HOST_PORT},CONTROLLER://0.0.0.0:${KAFKA_CONTROLLER_PORT} \
+  -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kafka:${KAFKA_PORT},PLAINTEXT_HOST://localhost:${KAFKA_HOST_PORT} \
+  -e KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT,CONTROLLER:PLAINTEXT \
+  -e KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER \
+  -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \
+  -e KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1 \
+  -e KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1 \
+  -e KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS=0 \
+  -e KAFKA_NUM_PARTITIONS=1"
 
 # Check if KAFKA_JMX_PORT is set and is a valid integer
 if [ -n "$KAFKA_JMX_PORT" ] && [ "$KAFKA_JMX_PORT" -eq "$KAFKA_JMX_PORT" ] 2>/dev/null; then
   # Valid integer; include JMX options
-  DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS --env JMX_PORT=$KAFKA_JMX_PORT --publish $KAFKA_JMX_PORT:$KAFKA_JMX_PORT"
+  DOCKER_RUN_OPTS+=" \
+     -e JMX_PORT=$KAFKA_JMX_PORT \
+     --publish $KAFKA_JMX_PORT:$KAFKA_JMX_PORT"
   log "Setting JMX_PORT to $KAFKA_JMX_PORT"
 else
   log "KAFKA_JMX_PORT not set or not a valid integer; skipping JMX_PORT configuration"
@@ -42,19 +61,9 @@ if [ -n "$KAFKA_DATA_DIR" ]; then
   mkdir -p "$KAFKA_DATA_DIR"
 
   # Add volume mount to Docker run options
-  DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS \
+  DOCKER_RUN_OPTS+=" \
     -v $KAFKA_DATA_DIR:/var/lib/kafka/data \
-    -e KAFKA_LOG_DIRS=/var/lib/kafka/data \
-    -e KAFKA_PROCESS_ROLES=broker,controller \
-    -e KAFKA_NODE_ID=1 \
-    -e KAFKA_CONTROLLER_QUORUM_VOTERS=1@kafka:9093 \
-    -e KAFKA_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 \
-    -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092 \
-    -e KAFKA_CONTROLLER_LISTENER_NAMES=CONTROLLER \
-    -e KAFKA_LOG_DIRS=/var/lib/kafka/data \
-    -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 \
-    -e KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1 \
-    -e KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1"
+    -e KAFKA_LOG_DIRS=/var/lib/kafka/data"
   log "Kafka data will be persisted in $KAFKA_DATA_DIR"
 else
   log "KAFKA_DATA_DIR not set; running Kafka with ephemeral container storage"
@@ -67,6 +76,7 @@ CONTAINER_ID=$(docker run \
   --detach \
   --network pal \
   --publish "$KAFKA_PORT":"$KAFKA_PORT" \
+  --publish "$KAFKA_HOST_PORT":"$KAFKA_HOST_PORT" \
   $DOCKER_RUN_OPTS apache/kafka:3.8.0)
 
 log "Kafka container started with ID: $CONTAINER_ID"
