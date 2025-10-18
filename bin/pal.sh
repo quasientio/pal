187de24d06184e2c11fadeda12d8d226eec24f72
Manuel Martinez <lucentum@gmail.com>
2025-10-18 12:59:58 +0200

    Add Chronicle-required --add-exports to `pal run` command

--- bin/pal.sh
+++ bin/pal.sh
@@ -38,6 +38,18 @@ JAVA_OPTS=${JAVA_OPTS:-""}
 if [ "$COMMAND" = "run" ]; then
   JAVA_OPTS="$JAVA_OPTS -server -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+ParallelRefProcEnabled -Xmx1g -XX:+IgnoreUnrecognizedVMOptions -Xverify:none"
 
+  CHRONICLE_EXPORTS="--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED \
+            --add-exports=java.base/sun.nio.ch=ALL-UNNAMED \
+            --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED \
+            --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
+            --add-opens=jdk.compiler/com.sun.tools.javac=ALL-UNNAMED \
+            --add-opens=java.base/java.lang=ALL-UNNAMED \
+            --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+            --add-opens=java.base/java.io=ALL-UNNAMED \
+            --add-opens=java.base/java.util=ALL-UNNAMED"
+
+  JAVA_OPTS="$JAVA_OPTS $CHRONICLE_EXPORTS"
+
   # Include JMX options if JMX_HOST and JMX_PORT are now set
   if [ -n "$JMX_HOST" ] && [ -n "$JMX_PORT" ]; then
     JAVA_OPTS="$JAVA_OPTS \
